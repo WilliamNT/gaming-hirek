b8c184ff3a6e43a94c0cd7697aa147e5bc6ae4d3
William <71773977+WilliamNT@users.noreply.github.com>
2021-11-14 13:02:52 +0100

    Add files via upload

--- gaming-hirek/app.py
+++ gaming-hirek/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from gnews import GNews
 
 app = Flask(__name__)
-google_news = GNews(language="hu", country="HU", period="3d", max_results=50)
-news = google_news.get_news(key="gamer hir")
+google_news = GNews(language="hu", country="HU", period="10d", max_results=50, exclude_websites=["globalgame.hu"]) # a globalgame-t az api hibásan tálalja, ezért inkább nem foglalkozunk vele
+news = google_news.get_news(key="gaming gamer hír videójáték játék játékok xbox playstation")
 
 @app.route("/")
 def home():
@@ -13,4 +13,4 @@ def home():
         )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
